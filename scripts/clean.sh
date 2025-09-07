5355e9ca7a17646d1c8fcdf9ccbdef70ad48f94e
jedster1111 <jedster1111@hotmail.co.uk>
2025-09-07 15:04:30 +0100

    Fix clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-cd "$parent_path"
-echo "Cleaning node_modules, dist and tsconfig.tsbuildinfo from $parent_path"
+cd "$parent_path/.."
+echo "Cleaning node_modules, dist and tsconfig.tsbuildinfo from $(pwd)"
 
 find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +
 find . -name 'dist' -type d -prune -exec rm -rf '{}' +
