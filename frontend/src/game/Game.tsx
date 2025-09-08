04041489bcfbca8734cb71fa8473f3270c2be170
jedster1111 <jedster1111@hotmail.co.uk>
2025-09-08 20:35:06 +0100

    Send json message

--- frontend/src/game/Game.tsx
+++ frontend/src/game/Game.tsx
@@ -1,32 +1,40 @@
 import { useCallback, type FC } from "react";
 import useWebSocket from "react-use-websocket";
 
+type Message<
+  Id extends string,
+  Payload extends Record<string, string | number> | undefined = undefined,
+> = { id: Id; payload: Payload };
+
+type HelloMessage = Message<"hello", undefined>;
+
+type Messages = HelloMessage;
+
 export const Game: FC = () => {
-  const { lastMessage, sendMessage } = useWebSocket("ws://localhost:3000/ws", {
-    shouldReconnect: () => true,
-    onMessage: (e) => {
-      console.log("Received message with data:", e.data);
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket<Messages>(
+    "ws://localhost:3000/ws",
+    {
+      shouldReconnect: () => true,
+      onMessage: (e) => {
+        console.log("Received message with data:", e.data);
+      },
     },
-  });
+  );
 
-  const handleClickSendMessageButton = useCallback(() => {
-    sendMessage("One small step for this client, and one huge step for...");
-  }, [sendMessage]);
+  const handleClickSendJsonButton = useCallback(() => {
+    sendJsonMessage({ id: "hello" });
+  }, [sendJsonMessage]);
 
-  const handleClickSendHelloServerButton = useCallback(() => {
-    sendMessage("hello");
-  }, [sendMessage]);
+  const lastMessageStringified =
+    JSON.stringify(lastJsonMessage, undefined, 2) || "NA";
 
   return (
     <div>
       <h1>Game!</h1>
-      <p>
-        Last message: {JSON.stringify(lastMessage?.data, undefined, 2) || "NA"}
-      </p>
-      <button onClick={handleClickSendMessageButton}>Send message</button>
-      <button onClick={handleClickSendHelloServerButton}>
-        Send hello message
-      </button>
+      <p>Last json message: {lastMessageStringified}</p>
+      <ul>
+        <button onClick={handleClickSendJsonButton}>Send json message</button>
+      </ul>
     </div>
   );
 };
--- servers/game/src/wsRoutes.ts
+++ servers/game/src/wsRoutes.ts
@@ -2,6 +2,15 @@ import type { FastifyPluginAsync } from "fastify";
 import { WebSocket } from "ws";
 import type { Data } from "ws";
 
+type Message<
+  Id extends string,
+  Payload extends Record<string, string | number> | undefined = undefined,
+> = { id: Id; payload: Payload };
+
+type HelloMessage = Message<"hello", undefined>;
+
+type Messages = HelloMessage;
+
 const wsRoutes: FastifyPluginAsync = async (fastify) => {
   fastify.get("/ws", { websocket: true }, (socket, req) => {
     fastify.log.info(`Client connected`);
@@ -14,12 +23,18 @@ const wsRoutes: FastifyPluginAsync = async (fastify) => {
 function messageHandler(this: WebSocket, message: Data) {
   console.log(`Received message: ${message}`);
 
-  switch (message.toString()) {
-    case "hello":
-      this.send("Why hello there...");
-      break;
-    default:
-      this.send("I don't know how to handle that!");
+  try {
+    const parsedMessage = JSON.parse(message.toString());
+    if (parsedMessage.id) {
+      this.send(
+        JSON.stringify({
+          id: "hello-back",
+          payload: { msg: "returning the favour" },
+        }),
+      );
+    }
+  } catch {
+    console.error("Whoops, failed to parse message");
   }
 }
 
